c7902aa92e36ffd254ae5eacca70072aee9cdfce
KwaneleNdaba <kwanelendaba113@gmail.com>
2023-12-20 15:03:08 +0200

    pins endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,81 @@
-import * as React from "react";
+import "./App.css";
 import Map, { Marker, Popup } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { Room, Star } from "@mui/icons-material";
-import "./App.css"
-import Navbar from "./components/Navbar";
-import Modal from "./components/Modal";
+import { useEffect, useState } from "react";
+import { Room, Star, StarBorder } from "@mui/icons-material";
+import axios from "axios";
+import { format } from "timeago.js";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 export default function App() {
-  const [showPopup, setShowPopup] = React.useState(true);
-  const [showRegister, setShowRegister] = React.useState(false);
-  const [showLogin, setShowLogin] = React.useState(false);
-  const [currentUsername, setCurrentUsername] = React.useState("");
+  const myStorage = window.localStorage;
+  const [currentUsername, setCurrentUsername] = useState(myStorage.getItem("user"));
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [star, setStar] = useState(0);
+  const [viewport, setViewport] = useState({
+    latitude: 47.040182,
+    longitude: 17.071727,
+    zoom: 4,
+  });
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+  };
+
+  const handleAddClick = (e) => {
+    const [longitude, latitude] = e.lngLat;
+    setNewPlace({
+      lat: latitude,
+      long: longitude,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUsername,
+      title,
+      desc,
+      rating: star,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
 
   const handleLogout = () => {
-
-  }
-
+    setCurrentUsername(null);
+    myStorage.removeItem("user");
+  };
   return (
     <div>
-     <Modal />
+  
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         initialViewState={{
@@ -30,44 +86,107 @@ export default function App() {
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
       >
-        <Marker
-          latitude={-5.89624}
-          longitude={22.41659}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-          <Room
-            onClick={() => setShowPopup((prev) => !prev)}
-            style={{ color: "red" }}
-          />
-        </Marker>
-        {showPopup && (
-          <Popup
-            longitude={22.41659}
-            latitude={-5.89624}
-            anchor="left"
-            onClose={() => setShowPopup(false)}
-          >
-            <div className="card">
-              <label>Place</label>
-              <h4 className="place">Kanaga</h4>
-              <label>Review</label>
-              <p>One of the most beautful countries in the world</p>
-              <label>Rating</label>
-            <div className="stars">
-            <Star/>
-              <Star/>
-              <Star/>
-              <Star/>
-              <Star/>
-            </div>
-              <label>Information</label>
-              <span className="username">Created by <b>Kwanele</b></span>
-              <span className="date">1 hour ago</span>
-            </div>
-          </Popup>
+       {pins.map((p) => (
+          <>
+            <Marker
+              latitude={p.lat}
+              longitude={p.long}
+              offsetLeft={-3.5 * viewport.zoom}
+              offsetTop={-7 * viewport.zoom}
+            >
+              <Room
+                style={{
+                  fontSize: 7 * viewport.zoom,
+                  color:
+                    currentUsername === p.username ? "tomato" : "slateblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+              />
+            </Marker>
+            {p._id === currentPlaceId && (
+              <Popup
+                key={p._id}
+                latitude={p.lat}
+                longitude={p.long}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setCurrentPlaceId(null)}
+                anchor="left"
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    {Array(p.rating).fill(<Star className="star" />)}
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
+        {newPlace && (
+          <>
+            <Marker
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              offsetLeft={-3.5 * viewport.zoom}
+              offsetTop={-7 * viewport.zoom}
+            >
+              <Room
+                style={{
+                  fontSize: 7 * viewport.zoom,
+                  color: "tomato",
+                  cursor: "pointer",
+                }}
+              />
+            </Marker>
+            <Popup
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setNewPlace(null)}
+              anchor="left"
+            >
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <label>Title</label>
+                  <input
+                    placeholder="Enter a title"
+                    autoFocus
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <label>Description</label>
+                  <textarea
+                    placeholder="Say us something about this place."
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+                  <label>Rating</label>
+                  <select onChange={(e) => setStar(e.target.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <button type="submit" className="submitButton">
+                    Add Pin
+                  </button>
+                </form>
+              </div>
+            </Popup>
+          </>
         )}
-         {currentUsername ? (
+        {currentUsername ? (
           <button className="button logout" onClick={handleLogout}>
             Log out
           </button>
@@ -84,7 +203,14 @@ export default function App() {
             </button>
           </div>
         )}
-      
+        {showRegister && <Register setShowRegister={setShowRegister} />}
+        {showLogin && (
+          <Login
+            setShowLogin={setShowLogin}
+            setCurrentUsername={setCurrentUsername}
+            myStorage={myStorage}
+          />
+        )}
       </Map>
     </div>
   );
